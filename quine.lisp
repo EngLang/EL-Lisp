94ba06d34a44dc9083e06fda73ed92c047954417
Cooper Pellaton <c@cepp.at>
2016-06-16 22:38:13 -0400

    Fixed the naming error.

--- quine.lisp
+++ quine.lisp
@@ -1,5 +1,5 @@
 (defun print-file ()
-    (let ((in (open "hello.lisp" :if-does-not-exist nil)))
+    (let ((in (open "quine.lisp" :if-does-not-exist nil)))
           (when in
             (loop for line = (read-line in nil)
                  while line do (format t "~a~%" line))
