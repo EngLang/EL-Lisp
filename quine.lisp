13f59097be74b535223016cbc250395b0a1b39ec
Aaron Vontell <vontell@mit.edu>
2016-06-16 22:30:51 -0400

    Made example quine

---
+++ quine.lisp
@@ -0,0 +1,9 @@
+(defun print-file ()
+    (let ((in (open "hello.lisp" :if-does-not-exist nil)))
+          (when in
+            (loop for line = (read-line in nil)
+                 while line do (format t "~a~%" line))
+            (close in)))
+)
+
+(print-file)
